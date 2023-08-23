7a2630d81fcf9696eb17f99c464bdb13dd65d712
ErikRodrigues-Dev <erik.rodrigues.dev@gmail.com>
2023-08-23 16:45:29 -0300

    Crud em andamento

---
+++ src/main/java/com/projetobackend/Projetobackend/repository/ProdutoRepostory.java
@@ -0,0 +1,75 @@
+package com.projetobackend.Projetobackend.repository;
+
+import com.projetobackend.Projetobackend.model.Produto;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class ProdutoRepostory {
+    private List<Produto> produtos = new ArrayList<Produto>();
+    private Integer ultimoId = 0;
+
+    /**
+     * Metodo para retornar uma lista de produtos
+     *
+     * @return Lista de produtos.
+     */
+    public List<Produto> obterTodos(){
+        return produtos;
+    }
+
+    /**
+     * Metodo para retornar uma lista de produtos pelo seu id.
+     * @param id do produto que será localizado.
+     * @return retorno um produto caso tenha encontrado
+     */
+    public Optional<Produto> obterPorId(Integer id){
+        return produtos
+                .stream()
+                .filter(produto -> produto.getId() == id)
+                .findFirst();
+    }
+
+    /**
+     * Metodo para adicionar produto na lista.
+     * @param produto que será adicionando.
+     * @return Retorna o produto que foi adicionado na lista.
+     */
+    public Produto adicionar(Produto produto){
+        ultimoId++;
+        produto.setId((ultimoId));
+        produto.add(produto);
+        return produto;
+    }
+
+    /**
+     * Método para deletar o produto por id.
+     * @param id do produto que vai ser deletado.
+     */
+    public void deletar(Integer id){
+        produtos.removeIf(produto -> produto.getId() == id);
+    }
+
+    /**
+     * Método para atualizar o produto na lista
+     * @param produto que será atualizado
+     * @return Retorna o produto apos atualizar a lista.
+     */
+    public Produto atualizar(Produto produto){
+       Optional<Produto> produtoEncontrado = obterTodos(produto.getId());
+
+       if (produtoEncontrado.isEmpty()){
+           throw new InputMismatchException("Produto não encontrado");
+       }
+       deletar(produto.getId());
+
+       produto.add(produto);
+
+       return produto;
+
+    }
+}
---
+++ src/test/java/com/projetobackend/Projetobackend/ProjetoBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetobackend.Projetobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
