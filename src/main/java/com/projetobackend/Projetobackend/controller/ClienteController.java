561378220974637a4b402c4c742624d606c91c8a
ErikRodrigues-Dev <erik.rodrigues.dev@gmail.com>
2023-08-28 16:13:22 -0300

    Classe Cliente com todos meotodos criados e funcionando. OBS: Classe criado só com consulta para aprimorar os conhecimentos do curso e entendimento

--- src/main/java/com/projetobackend/Projetobackend/controller/ClienteController.java
+++ src/main/java/com/projetobackend/Projetobackend/controller/ClienteController.java
@@ -1,7 +1,44 @@
 package com.projetobackend.Projetobackend.controller;
 
+import com.projetobackend.Projetobackend.model.Cliente;
+import com.projetobackend.Projetobackend.services.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/clientes")
 public class ClienteController {
 
+    @Autowired
+    public ClienteService clienteService;
 
+    @GetMapping
+    public List<Cliente> ObterTodos(){
+        return clienteService.obterTodos();
+    }
+
+    @PostMapping
+    public Cliente Adicionar(@RequestBody Cliente cliente){
+        return clienteService.adicionar(cliente);
+    }
+
+    @GetMapping("/{id}")
+    public Optional ObterPorId(@PathVariable Integer id){
+        return clienteService.obterPorId(id);
+    }
+
+    @DeleteMapping
+    public String deletar(@PathVariable Integer id){
+        clienteService.deletar(id);
+        return "Produto com id: "+id+"Foi deletado com sucesso!";
+    }
+
+    @PutMapping("/{id}")
+    public Cliente atualizar(@RequestBody Cliente cliente, @PathVariable Integer id){
+        return clienteService.atualizar(id, cliente);
+    }
 
 }
--- src/main/java/com/projetobackend/Projetobackend/services/ClienteService.java
+++ src/main/java/com/projetobackend/Projetobackend/services/ClienteService.java
@@ -1,9 +1,13 @@
 package com.projetobackend.Projetobackend.services;
 
 
+import com.projetobackend.Projetobackend.model.Cliente;
 import com.projetobackend.Projetobackend.repository.ClienteRepostory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ClienteService {
@@ -11,5 +15,18 @@ public class ClienteService {
     @Autowired
     private ClienteRepostory clienteRepostory;
 
-    
+    public List<Cliente>obterTodos(){return clienteRepostory.obterTodos();}
+
+    public Optional<Cliente>obterPorId(Integer id){return clienteRepostory.obterPorId(id);}
+
+    public Cliente adicionar (Cliente cliente){ return clienteRepostory.adicionar(cliente);}
+
+    public void deletar(Integer id){clienteRepostory.deletar(id);}
+
+    public Cliente atualizar(Integer id, Cliente cliente){
+        cliente.setCodCliente(id);
+        return clienteRepostory.atualizar(cliente);
+    }
+
+
 }
